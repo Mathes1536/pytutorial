16e3a1f42e9452653fa2cf0cd2c6827b258ab5c3
Matheswaran R <matheswaran1998@gmail.com>
2024-02-05 21:46:36 +0530

    til list

---
+++ 15-lists.py
@@ -0,0 +1,9 @@
+bag = list()
+bag.append('Soap')
+bag.append('Perfume')
+bag.append('99')
+
+bag.sort() # sorting error - not able to match with int & string
+
+print(bag, "\n")
+print('Mehtods of list ',"\n",dir(bag))
---
+++ 16-strings-with-list.py
@@ -0,0 +1,7 @@
+abc = ' my mail id is - test@email.com'
+split_string = abc.split()
+print(split_string)
+my_index = split_string.index('-')+1
+print('Your mail id is ',split_string[my_index])
+
+
